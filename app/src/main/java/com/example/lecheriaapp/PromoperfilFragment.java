c36bed9a9dae6b0bde7426348ff0df366efd7552
Sergio Alexander Huayllas Tirado <aogiri98a@gmail.com>
2023-04-28 14:04:57 -0500

    Se agrego promos, boton que reedirige al fragmento usa tu promo , y ubicanos

--- app/src/main/java/com/example/lecheriaapp/PromoperfilFragment.java
+++ app/src/main/java/com/example/lecheriaapp/PromoperfilFragment.java
@@ -4,60 +4,39 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PromoperfilFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PromoperfilFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public PromoperfilFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PromoperfilFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PromoperfilFragment newInstance(String param1, String param2) {
-        PromoperfilFragment fragment = new PromoperfilFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_promoperfil, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_promoperfil, container, false);
+
+        // Obtener el botón de "Quiero esta promo"
+        Button quieroPromoButton = rootView.findViewById(R.id.btn_quiero_promo);
+
+        // Establecer un OnClickListener para el botón
+        quieroPromoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Crear una instancia del fragmento PromocionSeleccionadaFragment
+                PromocionSeleccionadaFragment nuevoFragmento = new PromocionSeleccionadaFragment();
+
+                // Reemplazar el actual fragmento por el nuevo fragmento
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, nuevoFragmento);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        return rootView;
     }
 }
--- app/src/main/java/com/example/lecheriaapp/UbicanosFragment.java
+++ app/src/main/java/com/example/lecheriaapp/UbicanosFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
 
@@ -14,6 +16,17 @@ public class UbicanosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ubicanos, container, false);
+        View view = inflater.inflate(R.layout.fragment_ubicanos, container, false);
+
+        Spinner spinnerSede = (Spinner) view.findViewById(R.id.leftSpinnerSede);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                getContext(),
+                R.array.left_spinner_sedes,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerSede.setAdapter(adapter);
+
+        return view;
     }
 }
