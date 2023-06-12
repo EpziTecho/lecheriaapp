f2a354fd85d40218e3466ad67b838c82ad5d1a31
Franklin <fraya28427@gmail.com>
2023-06-11 23:04:49 -0500

    Avance de agregar producto y edditar producto

--- app/src/main/java/com/example/lecheriaapp/Vista/GestionProductosView/GestionProductosFragment.java
+++ app/src/main/java/com/example/lecheriaapp/Vista/GestionProductosView/GestionProductosFragment.java
@@ -1,10 +1,13 @@
 package com.example.lecheriaapp.Vista.GestionProductosView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -29,9 +32,13 @@ public class GestionProductosFragment extends Fragment implements  View.OnClickL
     private DatabaseReference mDatabase;
     private RecyclerView recyclerView;
     private ProductosHomePresenter productosHomePresenter;
+    private Spinner leftSpinnerEstado;
+    private Spinner leftSpinnerSede;
+    private Spinner leftSpinnerCategoria;
 
     public GestionProductosFragment() {
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,7 +48,27 @@ public class GestionProductosFragment extends Fragment implements  View.OnClickL
         mBtnAgregarProducto = view.findViewById(R.id.add_button);
         mBtnAgregarProducto.setOnClickListener(this);
         productosHomePresenter= new ProductosHomePresenter(getActivity(), mAuth, mDatabase);
-
+        /*
+        // Obtener la referencia a los Spinners del layout
+        leftSpinnerEstado = view.findViewById(R.id.textEstado1);
+        leftSpinnerSede = view.findViewById(R.id.textDisponibilidad1);
+        leftSpinnerCategoria = view.findViewById(R.id.textCategoria1);
+        // Crear un ArrayAdapter para cada Spinner con los datos deseados
+        ArrayAdapter<CharSequence> estadoAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.left_spinner_estado, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> sedeAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.left_spinner_items, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> categoriaAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.right_spinner_items, android.R.layout.simple_spinner_item);
+        // Especificar el diseño del menú desplegable de los Spinners
+        estadoAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sedeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categoriaAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Establecer los ArrayAdapter como adaptador de cada Spinner
+        leftSpinnerEstado.setAdapter(estadoAdapter);
+        leftSpinnerSede.setAdapter(sedeAdapter);
+        leftSpinnerCategoria.setAdapter(categoriaAdapter);
+         */
         return view;
     }
     @Override
